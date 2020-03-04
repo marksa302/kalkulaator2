91efb20401155cd596ab23ea0f447cd54b4a6587
ralfhei <ralf.heinsoo@gmail.com>
2020-03-05 01:05:32 +0200

    Desktop view and JS output

--- api.js
+++ api.js
@@ -1,1 +1,29 @@
+function addNew() 
+{
+    //output for Date
 
+    var newDate = document.createElement("p");
+    newDate.classList.add("dateField_1")
+    newDate.innerHTML = document.getElementById("dateTime").value
+    document.getElementById("ddOutput").appendChild(newDate)
+
+    // Output for description
+    
+    var newDesc = document.createElement("p");
+    newDesc.classList.add("descr_1")
+    newDesc.innerHTML = document.getElementById("descr").value
+    document.getElementById("dscrOutput").appendChild(newDesc)
+
+    // Output for time
+    
+    var newTime = document.createElement("p");
+    newTime.classList.add("time_1")
+    newTime.innerHTML = document.getElementById("time").value
+    document.getElementById("timeOutput").appendChild(newTime)
+
+    /*var newDesc = document.createElement("p");
+    newDesc.classList.add("_1")
+    newDesc.innerHTML = document.getElementById("descr").value
+    document.getElementById("dscrOutput").appendChild(newDesc)*/
+
+}
